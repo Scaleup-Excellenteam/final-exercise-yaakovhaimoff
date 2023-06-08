64e8dff628112e0032b0fd874dea75c89c5eb6c3
Yaakov Haimoff <hyaakov@aol.com>
2023-06-08 17:12:06 +0300

    added testing

--- ClientSide/Client.py
+++ ClientSide/Client.py
@@ -1,27 +1,76 @@
+from dataclasses import dataclass
 import requests
+from datetime import datetime
 
 
-def main():
-    while True:
-        file_path = input("Enter the file path (or 'quit' to exit): ")
+@dataclass
+class Status:
+    status: str
+    filename: str
+    timestamp: datetime
+    explanation: str
 
-        if file_path == 'quit':
-            break
+    def is_done(self):
+        return self.status == 'done'
 
+
+class ExplainerClient:
+    def __init__(self, base_url):
+        self.base_url = base_url
+
+    def upload(self, file_path):
         try:
             with open(file_path, 'rb') as file:
-                response = requests.post('http://127.0.0.1:5000/upload', files={'upload_file': file})
+                response = requests.post(f'{self.base_url}/upload', files={'upload_file': file})
                 if response.status_code == 200:
-                    print(response.json())
+                    response_data = response.json()
+                    uid = response_data['uid']
+                    return uid
                 else:
-                    print("File upload failed. Status code:", response.status_code)
+                    raise Exception("File upload failed. Status code:", response.status_code)
         except FileNotFoundError:
             print("File not found. Please enter a valid file path.")
         except IOError as e:
-            print("Error occurred while reading the file:", str(e))
+            print(str(e))
+
+    def status(self, uid):
+        try:
+            response = requests.get(f'{self.base_url}/status', params={'uid': uid})
+            if response.status_code == 200:
+                response_data = response.json()
+                status = response_data['status']
+                if status == 'not found' or status == 'pending':
+                    return Status(status, None, None, None)
+                filename = response_data['filename']
+                timestamp = datetime.fromtimestamp(int(response_data['timestamp']))
+                explanation = response_data['explanation']
+                return Status(status, filename, timestamp, explanation)
+            else:
+                raise Exception("Failed to get status. Status code:", response.status_code)
+        except IOError as e:
+            print(str(e))
+
+
+def main():
+    client = ExplainerClient('http://127.0.0.1:5000')
+
+    while True:
+        command = input("Enter 'upload' to upload file, status to check status of a file or 'quit' to exit): ")
+
+        if command == 'quit':
+            break
+        elif command == 'upload':
+            file_path = input("Enter the file path: ")
+            uid = client.upload(file_path)
+            print("UID:", uid)
+        elif command == 'status':
+            uid = input("Enter the UID: ")
+            status = client.status(uid)
+            print(status.is_done())
+            print(repr(status))
+        else:
+            print("Invalid command")
 
 
 if __name__ == '__main__':
     main()
-
-
--- README.md
+++ README.md
@@ -1,25 +1,44 @@
 ## Presentation Explanations From OpenAI
 
-
 ## Description
-This program processes a PowerPoint presentation and generates explanations 
-for each slide using the OpenAI GPT-3.5 Turbo model. 
-It extracts the text content from each slide and sends it
-to the OpenAI Chat API to obtain a response. 
-The responses are then saved as explanations for each slide.
 
+This program processes a PowerPoint presentation and generates explanations for each slide using the OpenAI GPT-3.5
+Turbo model. It extracts the text content from each slide and sends it to the OpenAI Chat API to obtain a response. The
+responses obtained from the API are then saved as explanations for each slide.
+
+The program consists of the following components:
+
+<ul>
+<li>Presentation Parser: Extracts the text content from each slide of the PowerPoint presentation.</li>
+<li>OpenAIAPI: Connects to the OpenAI Chat API and sends the extracted slide text to obtain responses.</li>
+<li>Explainer: Monitors the "uploads" folder for new presentations. When a new presentation is detected, 
+it utilizes the Presentation Parser and OpenAIAPI to generate explanations for each slide. 
+The explanations are then saved to the "outputs" folder.</li>
+<li>ServerAPI: Provides a web API interface to upload PowerPoint presentations and retrieve the processing status and generated explanations.</li>
+<li>Client: A client-side module that interacts with the ServerAPI to upload presentations and check their processing status.
+</li>
+</ul>
+
+The program creates an "uploads" folder where PowerPoint presentations can be uploaded for processing.
+The generated explanations are saved in the "outputs" folder.
+The ServerAPI allows users to monitor the processing status of uploaded presentations and retrieve the corresponding explanations.
 
 ## Usage
+
 In order to use the webb app you need to clone the project using this https url
+
 ```
 https://github.com/Scaleup-Excellenteam/final-exercise-yaakovhaimoff.git
 ```
 
 ## Run
+
 ```commandline
 python3 main.py presentation_file.pptx
 ```
+
 ## Contact info
+
 <ul>
 <li>Name: Yaakov Haimoff </li>
 <li>ID: 318528510 </li>
